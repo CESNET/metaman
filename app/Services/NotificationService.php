eb4e015af509daec5c281a4f5c4b61b5e380d1d2
Artem Otliaguzov <Artem.Otliaguzov@cesnet.cz>
2024-07-13 00:03:04 +0200

    make new NotificationService (not tested)

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -9,8 +9,15 @@ class NotificationService{
 
     public static function sendEntityNotification(Entity $entity,$notification){
         $admins = User::activeAdmins()->select('id', 'email')->get();
+
+        $operators = $entity->operators->pluck('id')->toArray();
+
+        $filteredAdmins = $admins->filter(function ($admin) use ($operators) {
+            return !in_array($admin->id, $operators);
+        });
+
         Notification::sendNow($entity->operators, new $notification($entity));
-        Notification::sendNow($admins, new $notification($entity));
+        Notification::sendNow($filteredAdmins, new $notification($entity));
     }
 
 }
