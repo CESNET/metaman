8a6aea5913b6878779147c639a593849bb10abdd
Jan Oppolzer <jan@oppolzer.cz>
2024-11-22 11:22:56 +0100

    Add defaults to config/storageCfg.php

--- config/storageCfg.php
+++ config/storageCfg.php
@@ -2,7 +2,7 @@
 
 return [
     'name' => 'metadata',
-    'mdaConfigFolder' => env('MDA_CONFIG_FOLDER'),
-    'mdaScript' => env('MDA_SCRIPT'),
-    'edu2edugain' => env('EDU_TO_EDUGAIN_FOLDER'),
+    'mdaConfigFolder' => env('MDA_CONFIG_FOLDER', '/opt/mda/config'),
+    'mdaScript' => env('MDA_SCRIPT', '/opt/mda/mda.sh'),
+    'edu2edugain' => env('EDU_TO_EDUGAIN_FOLDER', 'eduid2edugain'),
 ];
