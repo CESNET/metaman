81d7c14ed276b0767958d6c209b413d5e8e564c0
Jan Oppolzer <jan@oppolzer.cz>
2025-07-09 16:44:01 +0200

    Add EntityidController

---
+++ app/Http/Controllers/EntityidController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Ldap\CesnetOrganization;
+use App\Ldap\EduidczOrganization;
+use Illuminate\Http\Request;
+
+class EntityidController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        abort_unless($request->ico, 400, 'Missing company ID.');
+
+        $organization = CesnetOrganization::findBy('ico', $request->ico);
+        abort_unless($organization, 404, 'No organization found.');
+
+        $idp = EduidczOrganization::findBy('opointer', $organization->getDn());
+        abort_unless($idp, 404, 'No entityID.');
+
+        return response()->json([
+            'entityID' => $idp->getFirstAttribute('entityIDofIdP'),
+        ])->setEncodingOptions(JSON_UNESCAPED_SLASHES);
+    }
+}
