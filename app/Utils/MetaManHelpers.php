0b0cff97678c807836ec43917c665cf806ed421a
Jan Oppolzer <jan@oppolzer.cz>
2025-07-11 12:19:30 +0200

    Add return type and fix typo in MetaManHelpers.php

--- app/Utils/MetaManHelpers.php
+++ app/Utils/MetaManHelpers.php
@@ -1,7 +1,7 @@
 <?php
 
 if (! function_exists('generateFederationID')) {
-    function generateFederationID(string $name)
+    function generateFederationID(string $name): string
     {
         setlocale(LC_ALL, 'en_US.utf8');
 
@@ -9,7 +9,7 @@ if (! function_exists('generateFederationID')) {
         $ID = preg_replace('/\s+/', '-', $ID);              // Replace white spaces with a dash.
         $ID = preg_replace('/[^A-Za-z0-9-_]/', '', $ID);    // Drop everything except A-Z, a-z, 0-9, _ and -.
         $ID = preg_replace('/_{2,}/', '_', $ID);            // Replace two (or more) underscores with just one.
-        $ID = preg_replace('/-{2,}/', '-', $ID);            // Replace two (or more) dashed with just one.
+        $ID = preg_replace('/-{2,}/', '-', $ID);            // Replace two (or more) dashes with just one.
         $ID = strtolower($ID);
 
         return $ID;
